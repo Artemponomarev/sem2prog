8c01a52e2e7100fefceae0c75f8f77755592a4ee
Artemponomarev <artem.ponomarev444@gmail.com>
2017-02-09 19:04:45 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  testgithub
+//
+//  Created by Артем on 09.02.17.
+//  Copyright © 2017 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    //sdnfsfs
+    std::cout << "Hello "<<std::endl;
+    return 0;
+}
